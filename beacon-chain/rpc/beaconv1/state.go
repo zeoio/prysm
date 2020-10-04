c70235df676a57f658bc70bd94d88efb66424b04
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-10-04 18:06:45 -0400

    Begin val api impl

--- beacon-chain/rpc/beaconv1/state.go
+++ beacon-chain/rpc/beaconv1/state.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"errors"
 
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/state"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	ptypes "github.com/gogo/protobuf/types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1"
 )
@@ -27,4 +32,31 @@ func (bs *Server) GetStateFork(ctx context.Context, req *ethpb.StateRequest) (*e
 // not yet achieved, checkpoint should return epoch 0 and ZERO_HASH as root.
 func (bs *Server) GetFinalityCheckpoints(ctx context.Context, req *ethpb.StateRequest) (*ethpb.StateFinalityCheckpointResponse, error) {
 	return nil, errors.New("unimplemented")
+}
+
+State identifier. Can be one of: "head" (canonical head in node's view), "genesis", "finalized", "justified", <slot>, <hex encoded stateRoot with 0x prefix>.
+
+Example : head
+
+func (bs *Server) getState(stateId []byte) *state.BeaconState {
+	switch string(stateId) {
+	case "head":
+	case "genesis":
+	case "finalized":
+	case "justified":
+	default:
+		if len(bytes) == 32 {
+
+		} else {
+
+		}
+	}
+	startSlot, err := helpers.StartSlot(requestedEpoch)
+	if err != nil {
+		return nil, err
+	}
+	requestedState, err := bs.StateGen.StateBySlot(ctx, startSlot)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not get state")
+	}
 }
--- beacon-chain/rpc/beaconv1/validator.go
+++ beacon-chain/rpc/beaconv1/validator.go
@@ -3,6 +3,14 @@ package beaconv1
 import (
 	"context"
 	"errors"
+	"sort"
+	"strconv"
+
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/pagination"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1"
 )
@@ -19,6 +27,136 @@ func (bs *Server) ListValidators(ctx context.Context, req *ethpb.StateValidators
 
 // ListValidatorBalances returns a filterable list of validator balances.
 func (bs *Server) ListValidatorBalances(ctx context.Context, req *ethpb.StateValidatorsRequest) (*ethpb.ValidatorBalancesResponse, error) {
+	if bs.GenesisTimeFetcher == nil {
+		return nil, status.Errorf(codes.Internal, "Nil genesis time fetcher")
+	}
+	currentEpoch := helpers.SlotToEpoch(bs.GenesisTimeFetcher.CurrentSlot())
+	requestedEpoch := currentEpoch
+	switch q := req.QueryFilter.(type) {
+	case *ethpb.ListValidatorBalancesRequest_Epoch:
+		requestedEpoch = q.Epoch
+	case *ethpb.ListValidatorBalancesRequest_Genesis:
+		requestedEpoch = 0
+	}
+
+	if requestedEpoch > currentEpoch {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"Cannot retrieve information about an epoch in the future, current epoch %d, requesting %d",
+			currentEpoch,
+			requestedEpoch,
+		)
+	}
+	res := make([]*ethpb.ValidatorBalances_Balance, 0)
+	filtered := map[uint64]bool{} // Track filtered validators to prevent duplication in the response.
+
+	startSlot, err := helpers.StartSlot(requestedEpoch)
+	if err != nil {
+		return nil, err
+	}
+	requestedState, err := bs.StateGen.StateBySlot(ctx, startSlot)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not get state")
+	}
+
+	validators := requestedState.Validators()
+	balances := requestedState.Balances()
+	balancesCount := len(balances)
+	for _, pubKey := range req.PublicKeys {
+		// Skip empty public key.
+		if len(pubKey) == 0 {
+			continue
+		}
+		pubkeyBytes := bytesutil.ToBytes48(pubKey)
+		index, ok := requestedState.ValidatorIndexByPubkey(pubkeyBytes)
+		if !ok {
+			// We continue the loop if one validator in the request is not found.
+			res = append(res, &ethpb.ValidatorBalances_Balance{
+				Status: "UNKNOWN",
+			})
+			continue
+		}
+
+		filtered[index] = true
+
+		if index >= uint64(len(balances)) {
+			return nil, status.Errorf(codes.OutOfRange, "Validator index %d >= balance list %d",
+				index, len(balances))
+		}
+
+		res = append(res, &ethpb.ValidatorBalances_Balance{
+			PublicKey: pubKey,
+			Index:     index,
+			Balance:   balances[index],
+		})
+		balancesCount = len(res)
+	}
+
+	for _, index := range req.Indices {
+		if index >= uint64(len(balances)) {
+			return nil, status.Errorf(codes.OutOfRange, "Validator index %d >= balance list %d",
+				index, len(balances))
+		}
+
+		if !filtered[index] {
+			res = append(res, &ethpb.ValidatorBalances_Balance{
+				PublicKey: validators[index].PublicKey,
+				Index:     index,
+				Balance:   balances[index],
+			})
+		}
+		balancesCount = len(res)
+	}
+	// Depending on the indices and public keys given, results might not be sorted.
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].Index < res[j].Index
+	})
+
+	// If there are no balances, we simply return a response specifying this.
+	// Otherwise, attempting to paginate 0 balances below would result in an error.
+	if balancesCount == 0 {
+		return &ethpb.ValidatorBalances{
+			Epoch:         requestedEpoch,
+			Balances:      make([]*ethpb.ValidatorBalances_Balance, 0),
+			TotalSize:     int32(0),
+			NextPageToken: strconv.Itoa(0),
+		}, nil
+	}
+
+	start, end, nextPageToken, err := pagination.StartAndEndPage(req.PageToken, int(req.PageSize), balancesCount)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Could not paginate results: %v",
+			err,
+		)
+	}
+
+	if len(req.Indices) == 0 && len(req.PublicKeys) == 0 {
+		// Return everything.
+		for i := start; i < end; i++ {
+			pubkey := requestedState.PubkeyAtIndex(uint64(i))
+			res = append(res, &ethpb.ValidatorBalances_Balance{
+				PublicKey: pubkey[:],
+				Index:     uint64(i),
+				Balance:   balances[i],
+			})
+		}
+		return &ethpb.ValidatorBalances{
+			Epoch:         requestedEpoch,
+			Balances:      res,
+			TotalSize:     int32(balancesCount),
+			NextPageToken: nextPageToken,
+		}, nil
+	}
+
+	return &ethpb.ValidatorBalances{
+		Epoch:         requestedEpoch,
+		Balances:      res[start:end],
+		TotalSize:     int32(balancesCount),
+		NextPageToken: nextPageToken,
+	}, nil
+
 	return nil, errors.New("unimplemented")
 }
 
