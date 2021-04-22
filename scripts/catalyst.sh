10e4a5381062a7f1d602005abdbba5b5dae23886
Raul Jordan <raul@prysmaticlabs.com>
2021-04-21 23:33:52 -0500

    more progress on tool

---
+++ scripts/catalyst.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function color() {
+    # Usage: color "31;5" "string"
+    # Some valid values for color:
+    # - 5 blink, 1 strong, 4 underlined
+    # - fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
+    # - bg: 40 black, 41 red, 44 blue, 45 purple
+    printf '\033[%sm%s\033[0m\n' "$@"
+}
+
+if [[ $1 == init ]]; then
+    color "37" "Initializing catalyst"
+    bazel run //tools/catalyst --define=ssz=minimal -- -base-path=$(pwd) -state-output=$(pwd)/tools/catalyst/genesis.ssz
+fi
+
+if [[ $1 == geth ]]; then
+    color "37" "Launching geth"
+fi
+
+if [[ $1 == beacon-chain ]]; then
+    color "37" "Launching beacon-chain"
+fi
+
+if [[ $1 == validator ]]; then
+    color "37" "Launching validator"
+fi
--- tools/catalyst/main.go
+++ tools/catalyst/main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/google/uuid"
 	"github.com/minio/sha256-simd"
-	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/fileutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/validator/keymanager/derived"
+	"github.com/prysmaticlabs/prysm/validator/keymanager/imported"
 	"github.com/sirupsen/logrus"
 	"github.com/tyler-smith/go-bip39"
+	types "github.com/wealdtech/go-eth2-types/v2"
 	util "github.com/wealdtech/go-eth2-util"
+	keystorev4 "github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4"
 )
 
 const (
@@ -29,11 +37,25 @@ const (
 	eth2Config     = "tools/catalyst/eth2_config.yaml"
 )
 
-var log = logrus.WithField("prefix", "catalyst-tool")
+var (
+	log             = logrus.WithField("prefix", "catalyst-tool")
+	basePathFlag    = flag.String("base-path", "", "Base path for Prysm")
+	stateOutputFlag = flag.String("state-output", "", "State output path")
+)
+
+type accountStore struct {
+	PrivateKeys [][]byte `json:"private_keys"`
+	PublicKeys  [][]byte `json:"public_keys"`
+}
 
 func main() {
+	flag.Parse()
+	base, err := fileutil.ExpandPath(*basePathFlag)
+	if err != nil {
+		panic(err)
+	}
 	genesisTime := uint64(time.Now().Unix())
-	eth1Genesis, err := loadEth1GenesisConf(eth1Config)
+	eth1Genesis, err := loadEth1GenesisConf(filepath.Join(base, eth1Config))
 	if err != nil {
 		panic(err)
 	}
@@ -41,10 +63,40 @@ func main() {
 
 	eth1Db := rawdb.NewMemoryDatabase()
 	eth1GenesisBlock := eth1Genesis.ToBlock(eth1Db)
-	params.LoadChainConfigFile(eth2Config)
+	params.LoadChainConfigFile(filepath.Join(base, eth2Config))
 
-	validators, err := loadValidatorKeys()
+	validators, privKeys, pubKeys, err := loadValidatorKeys()
 	if err != nil {
+		panic(err)
+	}
+	accounts := &accountStore{
+		PrivateKeys: privKeys,
+		PublicKeys:  pubKeys,
+	}
+	encodedStore, err := json.MarshalIndent(accounts, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	encryptor := keystorev4.New()
+	id, err := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+	}
+	cryptoFields, err := encryptor.Encrypt(encodedStore, "foobar")
+	if err != nil {
+		panic(err)
+	}
+	ks := &imported.AccountsKeystoreRepresentation{
+		Crypto:  cryptoFields,
+		ID:      id.String(),
+		Version: encryptor.Version(),
+		Name:    encryptor.Name(),
+	}
+	encJSON, err := json.MarshalIndent(ks, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	if err := fileutil.WriteFile(filepath.Join(base, "tools", "catalyst", "wallet", "direct", "accounts", "all-accounts.keystore.json"), encJSON); err != nil {
 		panic(err)
 	}
 
@@ -102,21 +154,33 @@ func main() {
 		time.Unix(int64(t), 0),
 	)
 
-	//fmt.Println("done preparing state, serializing SSZ now...")
-	//f, err := os.OpenFile(g.StateOutputPath, os.O_CREATE|os.O_WRONLY, 0777)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer f.Close()
-	//buf := bufio.NewWriter(f)
-	//defer buf.Flush()
-	//w := codec.NewEncodingWriter(f)
-	//if err := state.Serialize(w); err != nil {
-	//	return err
-	//}
-	//fmt.Println("done!")
-
-	// Generates a beacon.config.yaml for Prysm's beacon node and a validator.config.yaml for the validator
+	f, err := os.OpenFile(*stateOutputFlag, os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	enc, err := beaconState.MarshalSSZ()
+	if err != nil {
+		panic(err)
+	}
+	buf := bufio.NewWriter(f)
+	defer func() {
+		if err := buf.Flush(); err != nil {
+			panic(err)
+		}
+	}()
+	n, err := buf.Write(enc)
+	if err != nil {
+		panic(err)
+	}
+	if n != len(enc) {
+		panic("Not equal length")
+	}
+	log.Info("Done!")
 }
 
 func loadEth1GenesisConf(configPath string) (*core.Genesis, error) {
@@ -131,26 +195,30 @@ func loadEth1GenesisConf(configPath string) (*core.Genesis, error) {
 	return &eth1Genesis, nil
 }
 
-func loadValidatorKeys() ([]*ethpb.Validator, error) {
+func loadValidatorKeys() (validators []*ethpb.Validator, privKeys, pubKeys [][]byte, err error) {
 	// Uses the provided mnemonic seed phrase to generate the
 	// appropriate seed file for recovering a derived wallets.
 	if ok := bip39.IsMnemonicValid(mnemonic); !ok {
 		panic(bip39.ErrInvalidMnemonic)
 	}
 	seed := bip39.NewSeed(strings.TrimSpace(mnemonic), "")
-	validators := make([]*ethpb.Validator, validatorCount)
+	validators = make([]*ethpb.Validator, validatorCount)
+	privKeys = make([][]byte, validatorCount)
+	pubKeys = make([][]byte, validatorCount)
 	for i := 0; i < validatorCount; i++ {
-		signingKey, err := util.PrivateKeyFromSeedAndPath(
+		var signingKey *types.BLSPrivateKey
+		signingKey, err = util.PrivateKeyFromSeedAndPath(
 			seed, fmt.Sprintf(derived.ValidatingKeyDerivationPathTemplate, i),
 		)
 		if err != nil {
-			return nil, errors.Wrap(err, "got bad withdrawal")
+			return
 		}
-		withdrawalKey, err := util.PrivateKeyFromSeedAndPath(
+		var withdrawalKey *types.BLSPrivateKey
+		withdrawalKey, err = util.PrivateKeyFromSeedAndPath(
 			seed, fmt.Sprintf(derived.WithdrawalKeyDerivationPathTemplate, i),
 		)
 		if err != nil {
-			return nil, err
+			return
 		}
 		validators[i] = &ethpb.Validator{
 			PublicKey:                  signingKey.PublicKey().Marshal(),
@@ -164,6 +232,9 @@ func loadValidatorKeys() ([]*ethpb.Validator, error) {
 		validators[i].WithdrawalCredentials = h.Sum(nil)
 		validators[i].WithdrawalCredentials[0] = params.BeaconConfig().BLSWithdrawalPrefixByte
 		validators[i].EffectiveBalance = params.BeaconConfig().MaxEffectiveBalance
+
+		privKeys[i] = signingKey.Marshal()
+		pubKeys[i] = signingKey.PublicKey().Marshal()
 	}
-	return validators, nil
+	return
 }
