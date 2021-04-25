d576881b24b1a8580a0932991bc621514092bd72
rauljordan <raul@prysmaticlabs.com>
2021-04-24 19:11:15 -0500

    everything matches

--- beacon-chain/powchain/deposit_trie_recreate_test.go
+++ beacon-chain/powchain/deposit_trie_recreate_test.go
@@ -2,6 +2,7 @@ package powchain
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/csv"
 	"encoding/hex"
 	"encoding/json"
@@ -10,14 +11,19 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
 	common2 "github.com/protolambda/zrnt/eth2/beacon/common"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/fileutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+	"github.com/prysmaticlabs/prysm/shared/trieutil"
 )
 
 type depositsMetadata struct {
@@ -35,6 +41,12 @@ type depositFileContents struct {
 	BlockNum     uint64               `json:"block_num"`
 }
 
+type depositContainer struct {
+	DepositIndex    uint64
+	DepositData     *ethpb.Deposit_Data
+	DepositDataRoot [32]byte
+}
+
 func TestRecreateDepositTrie(t *testing.T) {
 	base := "" // Change me
 	depositsMetadataCSVPath := filepath.Join(base, "deposit_tree_roots.csv")
@@ -43,20 +55,12 @@ func TestRecreateDepositTrie(t *testing.T) {
 	defer func() {
 		require.NoError(t, depositsMetadataCSV.Close())
 	}()
-	items := readCSVDepositsMetadata(t, depositsMetadataCSV)
-	_ = items
-	//
-	//depositsPath := filepath.Join(base, "deposits")
-	//trie, err := trieutil.NewTrie(params.BeaconConfig().DepositContractTreeDepth)
-	//require.NoError(t, err)
+	trieMetadataByDepositDataRoot := readCSVDepositsMetadata(t, depositsMetadataCSV)
 
-	// Try the first 10 deposts before attempting a large computation
-	// as a simple sanity test for our setup.
+	trie, err := trieutil.NewTrie(params.BeaconConfig().DepositContractTreeDepth)
+	require.NoError(t, err)
+
 	depositsPath := filepath.Join(base, "deposits")
-	readDeposits(t, depositsPath, 10)
-}
-
-func readDeposits(t *testing.T, depositsPath string, numDeposits int) {
 	files, err := ioutil.ReadDir(depositsPath)
 	require.NoError(t, err)
 	require.Equal(t, true, len(files) > 0)
@@ -67,19 +71,65 @@ func readDeposits(t *testing.T, depositsPath string, numDeposits int) {
 		}
 		filesInDir = append(filesInDir, files[i].Name())
 	}
-	filesInDir = filesInDir[:numDeposits]
+
+	deposits := make([]*depositContainer, 0)
 	for i := 0; i < len(filesInDir); i++ {
 		enc, err := fileutil.ReadFileAsBytes(filepath.Join(depositsPath, filesInDir[i]))
 		require.NoError(t, err)
-		var item *depositFileContents
-		err = json.Unmarshal(enc, &item)
+		var deposit *depositFileContents
+		err = json.Unmarshal(enc, &deposit)
 		require.NoError(t, err)
-		fmt.Printf("%+v", item)
-		fmt.Println("")
+		pubKeyStr := deposit.DepositData.Pubkey.String()
+		pubKeyBytes := hexDecodeOrDie(t, pubKeyStr)
+		sigStr := deposit.DepositData.Signature.String()
+		sigBytes := hexDecodeOrDie(t, sigStr)
+		withdrawalStr := deposit.DepositData.WithdrawalCredentials.String()
+		withdrawalBytes := hexDecodeOrDie(t, withdrawalStr)
+		depositData := &ethpb.Deposit_Data{
+			Amount:                uint64(deposit.DepositData.Amount),
+			PublicKey:             pubKeyBytes,
+			Signature:             sigBytes,
+			WithdrawalCredentials: withdrawalBytes,
+		}
+		depositRoot, err := depositData.HashTreeRoot()
+		require.NoError(t, err)
+
+		key := fmt.Sprintf("%x:%d", depositRoot, deposit.DepositIndex)
+		// Find it in the map.
+		_, ok := trieMetadataByDepositDataRoot[key]
+		if !ok {
+			// Not a valid deposit.
+			continue
+		}
+
+		deposits = append(deposits, &depositContainer{
+			DepositDataRoot: depositRoot,
+			DepositIndex:    uint64(deposit.DepositIndex),
+			DepositData:     depositData,
+		})
+	}
+
+	sort.Slice(deposits, func(i, j int) bool {
+		return deposits[i].DepositIndex < deposits[j].DepositIndex
+	})
+
+	for _, depositCntr := range deposits {
+		// Find it in the map.
+		key := fmt.Sprintf("%x:%d", depositCntr.DepositDataRoot, depositCntr.DepositIndex)
+		metadata, ok := trieMetadataByDepositDataRoot[key]
+		require.Equal(t, true, ok)
+
+		t.Logf("Inserting deposit with index %d and deposit data root %#x", depositCntr.DepositIndex, depositCntr.DepositDataRoot)
+		trie.Insert(depositCntr.DepositDataRoot[:], int(depositCntr.DepositIndex))
+		receivedTrieRoot := trie.Root()
+		wantedRoot := metadata.DepositTreeRoot
+		if !bytes.Equal(wantedRoot, receivedTrieRoot[:]) {
+			t.Fatalf("Wanted deposit trie root %#x for deposit index %d and deposit data root %#x, received %#x as the deposit trie root", wantedRoot, depositCntr.DepositIndex, depositCntr.DepositDataRoot, receivedTrieRoot)
+		}
 	}
 }
 
-func readCSVDepositsMetadata(t *testing.T, rs io.ReadSeeker) []*depositsMetadata {
+func readCSVDepositsMetadata(t *testing.T, rs io.ReadSeeker) map[string]*depositsMetadata {
 	// Skip first row of headers.
 	row1, err := bufio.NewReader(rs).ReadSlice('\n')
 	require.NoError(t, err)
@@ -90,14 +140,15 @@ func readCSVDepositsMetadata(t *testing.T, rs io.ReadSeeker) []*depositsMetadata
 	r := csv.NewReader(rs)
 	rows, err := r.ReadAll()
 	require.NoError(t, err)
-	results := make([]*depositsMetadata, len(rows))
+	trieMetadataByDepositDataRoot := make(map[string]*depositsMetadata)
 	for i := 0; i < len(rows); i++ {
 		// deposit_index,deposit_data_root,deposit_tree_root,deposit_tree_contents_root,block_hash
 		data := rows[i]
 		depositIdx, err := strconv.Atoi(data[0])
 		require.NoError(t, err)
 
-		results[i] = &depositsMetadata{
+		key := fmt.Sprintf("%x:%d", bytesutil.ToBytes32(hexDecodeOrDie(t, data[1])), depositIdx)
+		trieMetadataByDepositDataRoot[key] = &depositsMetadata{
 			DepositIndex:            uint64(depositIdx),
 			DepositDataRoot:         hexDecodeOrDie(t, data[1]),
 			DepositTreeRoot:         hexDecodeOrDie(t, data[2]),
@@ -105,7 +156,7 @@ func readCSVDepositsMetadata(t *testing.T, rs io.ReadSeeker) []*depositsMetadata
 			BlockHash:               hexDecodeOrDie(t, data[4]),
 		}
 	}
-	return results
+	return trieMetadataByDepositDataRoot
 }
 
 func hexDecodeOrDie(t *testing.T, hexStr string) []byte {
