82e51199c5065dfc156a7d99d37130cebce04546
terence tsao <terence@prysmaticlabs.com>
2021-06-05 14:10:30 -0700

    Add shard work

--- proto/beacon/p2p/v1/types.pb.go
+++ proto/beacon/p2p/v1/types.pb.go
@@ -1383,6 +1383,147 @@ func (x *SyncAggregatorSelectionData) GetSubcommitteeIndex() uint64 {
 	return 0
 }
 
+type ShardWork struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Types that are assignable to Status:
+	//	*ShardWork_None
+	//	*ShardWork_DataCommitment
+	//	*ShardWork_PendingShardHeaders
+	Status isShardWork_Status `protobuf_oneof:"status"`
+}
+
+func (x *ShardWork) Reset() {
+	*x = ShardWork{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_beacon_p2p_v1_types_proto_msgTypes[15]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ShardWork) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ShardWork) ProtoMessage() {}
+
+func (x *ShardWork) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_beacon_p2p_v1_types_proto_msgTypes[15]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ShardWork.ProtoReflect.Descriptor instead.
+func (*ShardWork) Descriptor() ([]byte, []int) {
+	return file_proto_beacon_p2p_v1_types_proto_rawDescGZIP(), []int{15}
+}
+
+func (m *ShardWork) GetStatus() isShardWork_Status {
+	if m != nil {
+		return m.Status
+	}
+	return nil
+}
+
+func (x *ShardWork) GetNone() []byte {
+	if x, ok := x.GetStatus().(*ShardWork_None); ok {
+		return x.None
+	}
+	return nil
+}
+
+func (x *ShardWork) GetDataCommitment() *v1alpha1.DataCommitment {
+	if x, ok := x.GetStatus().(*ShardWork_DataCommitment); ok {
+		return x.DataCommitment
+	}
+	return nil
+}
+
+func (x *ShardWork) GetPendingShardHeaders() *PendingShardHeaders {
+	if x, ok := x.GetStatus().(*ShardWork_PendingShardHeaders); ok {
+		return x.PendingShardHeaders
+	}
+	return nil
+}
+
+type isShardWork_Status interface {
+	isShardWork_Status()
+}
+
+type ShardWork_None struct {
+	None []byte `protobuf:"bytes,1,opt,name=none,proto3,oneof"`
+}
+
+type ShardWork_DataCommitment struct {
+	DataCommitment *v1alpha1.DataCommitment `protobuf:"bytes,2,opt,name=data_commitment,json=dataCommitment,proto3,oneof"`
+}
+
+type ShardWork_PendingShardHeaders struct {
+	PendingShardHeaders *PendingShardHeaders `protobuf:"bytes,3,opt,name=pending_shard_headers,json=pendingShardHeaders,proto3,oneof"`
+}
+
+func (*ShardWork_None) isShardWork_Status() {}
+
+func (*ShardWork_DataCommitment) isShardWork_Status() {}
+
+func (*ShardWork_PendingShardHeaders) isShardWork_Status() {}
+
+type PendingShardHeaders struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	PendingShardHeaders []*PendingShardHeader `protobuf:"bytes,1,rep,name=pending_shard_headers,json=pendingShardHeaders,proto3" json:"pending_shard_headers,omitempty" ssz-max:"4"`
+}
+
+func (x *PendingShardHeaders) Reset() {
+	*x = PendingShardHeaders{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_beacon_p2p_v1_types_proto_msgTypes[16]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PendingShardHeaders) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PendingShardHeaders) ProtoMessage() {}
+
+func (x *PendingShardHeaders) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_beacon_p2p_v1_types_proto_msgTypes[16]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PendingShardHeaders.ProtoReflect.Descriptor instead.
+func (*PendingShardHeaders) Descriptor() ([]byte, []int) {
+	return file_proto_beacon_p2p_v1_types_proto_rawDescGZIP(), []int{16}
+}
+
+func (x *PendingShardHeaders) GetPendingShardHeaders() []*PendingShardHeader {
+	if x != nil {
+		return x.PendingShardHeaders
+	}
+	return nil
+}
+
 var File_proto_beacon_p2p_v1_types_proto protoreflect.FileDescriptor
 
 var file_proto_beacon_p2p_v1_types_proto_rawDesc = []byte{
@@ -1793,7 +1934,29 @@ var file_proto_beacon_p2p_v1_types_proto_rawDesc = []byte{
 	0x6f, 0x74, 0x12, 0x2d, 0x0a, 0x12, 0x73, 0x75, 0x62, 0x63, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x74,
 	0x65, 0x65, 0x5f, 0x69, 0x6e, 0x64, 0x65, 0x78, 0x18, 0x02, 0x20, 0x01, 0x28, 0x04, 0x52, 0x11,
 	0x73, 0x75, 0x62, 0x63, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x74, 0x65, 0x65, 0x49, 0x6e, 0x64, 0x65,
-	0x78, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x78, 0x22, 0xe0, 0x01, 0x0a, 0x09, 0x53, 0x68, 0x61, 0x72, 0x64, 0x57, 0x6f, 0x72, 0x6b, 0x12,
+	0x14, 0x0a, 0x04, 0x6e, 0x6f, 0x6e, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x48, 0x00, 0x52,
+	0x04, 0x6e, 0x6f, 0x6e, 0x65, 0x12, 0x50, 0x0a, 0x0f, 0x64, 0x61, 0x74, 0x61, 0x5f, 0x63, 0x6f,
+	0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x25,
+	0x2e, 0x65, 0x74, 0x68, 0x65, 0x72, 0x65, 0x75, 0x6d, 0x2e, 0x65, 0x74, 0x68, 0x2e, 0x76, 0x31,
+	0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x2e, 0x44, 0x61, 0x74, 0x61, 0x43, 0x6f, 0x6d, 0x6d, 0x69,
+	0x74, 0x6d, 0x65, 0x6e, 0x74, 0x48, 0x00, 0x52, 0x0e, 0x64, 0x61, 0x74, 0x61, 0x43, 0x6f, 0x6d,
+	0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x12, 0x61, 0x0a, 0x15, 0x70, 0x65, 0x6e, 0x64, 0x69,
+	0x6e, 0x67, 0x5f, 0x73, 0x68, 0x61, 0x72, 0x64, 0x5f, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x73,
+	0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x2b, 0x2e, 0x65, 0x74, 0x68, 0x65, 0x72, 0x65, 0x75,
+	0x6d, 0x2e, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x70, 0x32, 0x70, 0x2e, 0x76, 0x31, 0x2e,
+	0x50, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x53, 0x68, 0x61, 0x72, 0x64, 0x48, 0x65, 0x61, 0x64,
+	0x65, 0x72, 0x73, 0x48, 0x00, 0x52, 0x13, 0x70, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x53, 0x68,
+	0x61, 0x72, 0x64, 0x48, 0x65, 0x61, 0x64, 0x65, 0x72, 0x73, 0x42, 0x08, 0x0a, 0x06, 0x73, 0x74,
+	0x61, 0x74, 0x75, 0x73, 0x22, 0x7c, 0x0a, 0x13, 0x50, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x53,
+	0x68, 0x61, 0x72, 0x64, 0x48, 0x65, 0x61, 0x64, 0x65, 0x72, 0x73, 0x12, 0x65, 0x0a, 0x15, 0x70,
+	0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x5f, 0x73, 0x68, 0x61, 0x72, 0x64, 0x5f, 0x68, 0x65, 0x61,
+	0x64, 0x65, 0x72, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x2a, 0x2e, 0x65, 0x74, 0x68,
+	0x65, 0x72, 0x65, 0x75, 0x6d, 0x2e, 0x62, 0x65, 0x61, 0x63, 0x6f, 0x6e, 0x2e, 0x70, 0x32, 0x70,
+	0x2e, 0x76, 0x31, 0x2e, 0x50, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x53, 0x68, 0x61, 0x72, 0x64,
+	0x48, 0x65, 0x61, 0x64, 0x65, 0x72, 0x42, 0x05, 0x92, 0xb5, 0x18, 0x01, 0x34, 0x52, 0x13, 0x70,
+	0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x53, 0x68, 0x61, 0x72, 0x64, 0x48, 0x65, 0x61, 0x64, 0x65,
+	0x72, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -1808,7 +1971,7 @@ func file_proto_beacon_p2p_v1_types_proto_rawDescGZIP() []byte {
 	return file_proto_beacon_p2p_v1_types_proto_rawDescData
 }
 
-var file_proto_beacon_p2p_v1_types_proto_msgTypes = make([]protoimpl.MessageInfo, 15)
+var file_proto_beacon_p2p_v1_types_proto_msgTypes = make([]protoimpl.MessageInfo, 17)
 var file_proto_beacon_p2p_v1_types_proto_goTypes = []interface{}{
 	(*BeaconState)(nil),                 // 0: ethereum.beacon.p2p.v1.BeaconState
 	(*BeaconStateAltair)(nil),           // 1: ethereum.beacon.p2p.v1.BeaconStateAltair
@@ -1825,47 +1988,52 @@ var file_proto_beacon_p2p_v1_types_proto_goTypes = []interface{}{
 	(*EpochDataCommitments)(nil),        // 12: ethereum.beacon.p2p.v1.EpochDataCommitments
 	(*SyncCommittee)(nil),               // 13: ethereum.beacon.p2p.v1.SyncCommittee
 	(*SyncAggregatorSelectionData)(nil), // 14: ethereum.beacon.p2p.v1.SyncAggregatorSelectionData
-	(*v1alpha1.BeaconBlockHeader)(nil),  // 15: ethereum.eth.v1alpha1.BeaconBlockHeader
-	(*v1alpha1.Eth1Data)(nil),           // 16: ethereum.eth.v1alpha1.Eth1Data
-	(*v1alpha1.Validator)(nil),          // 17: ethereum.eth.v1alpha1.Validator
-	(*v1alpha1.Checkpoint)(nil),         // 18: ethereum.eth.v1alpha1.Checkpoint
-	(*v1alpha1.AttestationData)(nil),    // 19: ethereum.eth.v1alpha1.AttestationData
-	(*v1alpha1.DataCommitment)(nil),     // 20: ethereum.eth.v1alpha1.DataCommitment
+	(*ShardWork)(nil),                   // 15: ethereum.beacon.p2p.v1.ShardWork
+	(*PendingShardHeaders)(nil),         // 16: ethereum.beacon.p2p.v1.PendingShardHeaders
+	(*v1alpha1.BeaconBlockHeader)(nil),  // 17: ethereum.eth.v1alpha1.BeaconBlockHeader
+	(*v1alpha1.Eth1Data)(nil),           // 18: ethereum.eth.v1alpha1.Eth1Data
+	(*v1alpha1.Validator)(nil),          // 19: ethereum.eth.v1alpha1.Validator
+	(*v1alpha1.Checkpoint)(nil),         // 20: ethereum.eth.v1alpha1.Checkpoint
+	(*v1alpha1.AttestationData)(nil),    // 21: ethereum.eth.v1alpha1.AttestationData
+	(*v1alpha1.DataCommitment)(nil),     // 22: ethereum.eth.v1alpha1.DataCommitment
 }
 var file_proto_beacon_p2p_v1_types_proto_depIdxs = []int32{
 	2,  // 0: ethereum.beacon.p2p.v1.BeaconState.fork:type_name -> ethereum.beacon.p2p.v1.Fork
-	15, // 1: ethereum.beacon.p2p.v1.BeaconState.latest_block_header:type_name -> ethereum.eth.v1alpha1.BeaconBlockHeader
-	16, // 2: ethereum.beacon.p2p.v1.BeaconState.eth1_data:type_name -> ethereum.eth.v1alpha1.Eth1Data
-	16, // 3: ethereum.beacon.p2p.v1.BeaconState.eth1_data_votes:type_name -> ethereum.eth.v1alpha1.Eth1Data
-	17, // 4: ethereum.beacon.p2p.v1.BeaconState.validators:type_name -> ethereum.eth.v1alpha1.Validator
+	17, // 1: ethereum.beacon.p2p.v1.BeaconState.latest_block_header:type_name -> ethereum.eth.v1alpha1.BeaconBlockHeader
+	18, // 2: ethereum.beacon.p2p.v1.BeaconState.eth1_data:type_name -> ethereum.eth.v1alpha1.Eth1Data
+	18, // 3: ethereum.beacon.p2p.v1.BeaconState.eth1_data_votes:type_name -> ethereum.eth.v1alpha1.Eth1Data
+	19, // 4: ethereum.beacon.p2p.v1.BeaconState.validators:type_name -> ethereum.eth.v1alpha1.Validator
 	3,  // 5: ethereum.beacon.p2p.v1.BeaconState.previous_epoch_attestations:type_name -> ethereum.beacon.p2p.v1.PendingAttestation
 	3,  // 6: ethereum.beacon.p2p.v1.BeaconState.current_epoch_attestations:type_name -> ethereum.beacon.p2p.v1.PendingAttestation
-	18, // 7: ethereum.beacon.p2p.v1.BeaconState.previous_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
-	18, // 8: ethereum.beacon.p2p.v1.BeaconState.current_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
-	18, // 9: ethereum.beacon.p2p.v1.BeaconState.finalized_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
+	20, // 7: ethereum.beacon.p2p.v1.BeaconState.previous_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
+	20, // 8: ethereum.beacon.p2p.v1.BeaconState.current_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
+	20, // 9: ethereum.beacon.p2p.v1.BeaconState.finalized_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
 	10, // 10: ethereum.beacon.p2p.v1.BeaconState.latest_execution_payload_header:type_name -> ethereum.beacon.p2p.v1.ExecutionPayloadHeader
 	11, // 11: ethereum.beacon.p2p.v1.BeaconState.previous_epoch_pending_shard_headers:type_name -> ethereum.beacon.p2p.v1.PendingShardHeader
 	11, // 12: ethereum.beacon.p2p.v1.BeaconState.current_epoch_pending_shard_headers:type_name -> ethereum.beacon.p2p.v1.PendingShardHeader
 	12, // 13: ethereum.beacon.p2p.v1.BeaconState.grandparent_epoch_confirmed_commitments:type_name -> ethereum.beacon.p2p.v1.EpochDataCommitments
 	2,  // 14: ethereum.beacon.p2p.v1.BeaconStateAltair.fork:type_name -> ethereum.beacon.p2p.v1.Fork
-	15, // 15: ethereum.beacon.p2p.v1.BeaconStateAltair.latest_block_header:type_name -> ethereum.eth.v1alpha1.BeaconBlockHeader
-	16, // 16: ethereum.beacon.p2p.v1.BeaconStateAltair.eth1_data:type_name -> ethereum.eth.v1alpha1.Eth1Data
-	16, // 17: ethereum.beacon.p2p.v1.BeaconStateAltair.eth1_data_votes:type_name -> ethereum.eth.v1alpha1.Eth1Data
-	17, // 18: ethereum.beacon.p2p.v1.BeaconStateAltair.validators:type_name -> ethereum.eth.v1alpha1.Validator
-	18, // 19: ethereum.beacon.p2p.v1.BeaconStateAltair.previous_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
-	18, // 20: ethereum.beacon.p2p.v1.BeaconStateAltair.current_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
-	18, // 21: ethereum.beacon.p2p.v1.BeaconStateAltair.finalized_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
+	17, // 15: ethereum.beacon.p2p.v1.BeaconStateAltair.latest_block_header:type_name -> ethereum.eth.v1alpha1.BeaconBlockHeader
+	18, // 16: ethereum.beacon.p2p.v1.BeaconStateAltair.eth1_data:type_name -> ethereum.eth.v1alpha1.Eth1Data
+	18, // 17: ethereum.beacon.p2p.v1.BeaconStateAltair.eth1_data_votes:type_name -> ethereum.eth.v1alpha1.Eth1Data
+	19, // 18: ethereum.beacon.p2p.v1.BeaconStateAltair.validators:type_name -> ethereum.eth.v1alpha1.Validator
+	20, // 19: ethereum.beacon.p2p.v1.BeaconStateAltair.previous_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
+	20, // 20: ethereum.beacon.p2p.v1.BeaconStateAltair.current_justified_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
+	20, // 21: ethereum.beacon.p2p.v1.BeaconStateAltair.finalized_checkpoint:type_name -> ethereum.eth.v1alpha1.Checkpoint
 	13, // 22: ethereum.beacon.p2p.v1.BeaconStateAltair.current_sync_committee:type_name -> ethereum.beacon.p2p.v1.SyncCommittee
 	13, // 23: ethereum.beacon.p2p.v1.BeaconStateAltair.next_sync_committee:type_name -> ethereum.beacon.p2p.v1.SyncCommittee
-	19, // 24: ethereum.beacon.p2p.v1.PendingAttestation.data:type_name -> ethereum.eth.v1alpha1.AttestationData
+	21, // 24: ethereum.beacon.p2p.v1.PendingAttestation.data:type_name -> ethereum.eth.v1alpha1.AttestationData
 	2,  // 25: ethereum.beacon.p2p.v1.CheckPtInfo.fork:type_name -> ethereum.beacon.p2p.v1.Fork
-	20, // 26: ethereum.beacon.p2p.v1.PendingShardHeader.commitment:type_name -> ethereum.eth.v1alpha1.DataCommitment
-	20, // 27: ethereum.beacon.p2p.v1.EpochDataCommitments.data_commitments:type_name -> ethereum.eth.v1alpha1.DataCommitment
-	28, // [28:28] is the sub-list for method output_type
-	28, // [28:28] is the sub-list for method input_type
-	28, // [28:28] is the sub-list for extension type_name
-	28, // [28:28] is the sub-list for extension extendee
-	0,  // [0:28] is the sub-list for field type_name
+	22, // 26: ethereum.beacon.p2p.v1.PendingShardHeader.commitment:type_name -> ethereum.eth.v1alpha1.DataCommitment
+	22, // 27: ethereum.beacon.p2p.v1.EpochDataCommitments.data_commitments:type_name -> ethereum.eth.v1alpha1.DataCommitment
+	22, // 28: ethereum.beacon.p2p.v1.ShardWork.data_commitment:type_name -> ethereum.eth.v1alpha1.DataCommitment
+	16, // 29: ethereum.beacon.p2p.v1.ShardWork.pending_shard_headers:type_name -> ethereum.beacon.p2p.v1.PendingShardHeaders
+	11, // 30: ethereum.beacon.p2p.v1.PendingShardHeaders.pending_shard_headers:type_name -> ethereum.beacon.p2p.v1.PendingShardHeader
+	31, // [31:31] is the sub-list for method output_type
+	31, // [31:31] is the sub-list for method input_type
+	31, // [31:31] is the sub-list for extension type_name
+	31, // [31:31] is the sub-list for extension extendee
+	0,  // [0:31] is the sub-list for field type_name
 }
 
 func init() { file_proto_beacon_p2p_v1_types_proto_init() }
@@ -2054,6 +2222,35 @@ func file_proto_beacon_p2p_v1_types_proto_init() {
 				return nil
 			}
 		}
+		file_proto_beacon_p2p_v1_types_proto_msgTypes[15].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ShardWork); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_beacon_p2p_v1_types_proto_msgTypes[16].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PendingShardHeaders); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
+	file_proto_beacon_p2p_v1_types_proto_msgTypes[15].OneofWrappers = []interface{}{
+		(*ShardWork_None)(nil),
+		(*ShardWork_DataCommitment)(nil),
+		(*ShardWork_PendingShardHeaders)(nil),
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -2061,7 +2258,7 @@ func file_proto_beacon_p2p_v1_types_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_beacon_p2p_v1_types_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   15,
+			NumMessages:   17,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
