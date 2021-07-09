60bb82dbbe08240385ec35be7090cad03d0530d3
Raul Jordan <raul@prysmaticlabs.com>
2021-07-08 22:38:29 -0500

    begin backfilling routine

--- beacon-chain/slasher/service.go
+++ beacon-chain/slasher/service.go
@@ -11,11 +11,16 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/blockchain"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/feed"
 	statefeed "github.com/prysmaticlabs/prysm/beacon-chain/core/feed/state"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/slashings"
+	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stategen"
 	"github.com/prysmaticlabs/prysm/beacon-chain/sync"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/proto/interfaces"
+	"github.com/prysmaticlabs/prysm/shared/attestationutil"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/event"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/slotutil"
@@ -112,7 +117,11 @@ func (s *Service) run() {
 	secondsPerSlot := params.BeaconConfig().SecondsPerSlot
 	s.slotTicker = slotutil.NewSlotTicker(s.genesisTime, secondsPerSlot)
 
-	s.waitForSync(s.genesisTime)
+	// Wait until the beacon node is synced (short cirtuits if genesis epoch).
+	s.waitForSync(s.ctx, s.genesisTime)
+
+	// Perform a backfilling process for data from the synced epoch, N, down to N - WEAK_SUBJECTIVITY_PERIOD.
+	s.backfillSlasherHistory(s.ctx)
 
 	log.Info("Completed chain sync, starting slashing detection")
 	go s.processQueuedAttestations(s.ctx, s.slotTicker.C())
@@ -136,7 +145,68 @@ func (s *Service) Status() error {
 	return nil
 }
 
-func (s *Service) waitForSync(genesisTime time.Time) {
+func (s *Service) backfillSlasherHistory(ctx context.Context) {
+	// Perform backfilling in chunks of N blocks at a time
+	blocksOnDisk := make([]interfaces.BeaconBlock, 0)
+	blockWrappers := make([]*slashertypes.SignedBlockHeaderWrapper, 0)
+	attWrappers := make([]*slashertypes.IndexedAttestationWrapper, 0)
+	for _, blk := range blocksOnDisk {
+		preState, err := s.serviceCfg.StateGen.StateByRoot(ctx, bytesutil.ToBytes32(blk.ParentRoot()))
+		if err != nil {
+			return
+		}
+		if preState == nil || preState.IsNil() {
+			return
+		}
+		for _, att := range blk.Body().Attestations() {
+			committee, err := helpers.BeaconCommitteeFromState(preState, att.Data.Slot, att.Data.CommitteeIndex)
+			if err != nil {
+				log.WithError(err).Error("Could not get attestation committee")
+				return
+			}
+			// Using a different context to prevent timeouts as this operation can be expensive
+			// and we want to avoid affecting the critical code path.
+			indexedAtt, err := attestationutil.ConvertToIndexed(ctx, att, committee)
+			if err != nil {
+				log.WithError(err).Error("Could not convert to indexed attestation")
+				return
+			}
+			signingRoot, err := att.Data.HashTreeRoot()
+			if err != nil {
+				log.WithError(err).Error("Could not get hash tree root of attestation")
+				continue
+			}
+			attWrapper := &slashertypes.IndexedAttestationWrapper{
+				IndexedAttestation: indexedAtt,
+				SigningRoot:        signingRoot,
+			}
+			attWrappers = append(attWrappers, attWrapper)
+		}
+	}
+
+	// TODO: Need to also save the blocks and attestations in slasher's database.
+
+	propSlashings, err := s.detectProposerSlashings(ctx, blockWrappers)
+	if err != nil {
+		log.WithError(err).Error("Could not detect proposer slashings on backfilled data")
+		return
+	}
+	attSlashings, err := s.checkSlashableAttestations(ctx, attWrappers)
+	if err != nil {
+		log.WithError(err).Error("Could not detect proposer slashings on backfilled data")
+		return
+	}
+	if err := s.processProposerSlashings(ctx, propSlashings); err != nil {
+		log.WithError(err).Error("Could not process proposer slashings")
+		return
+	}
+	if err := s.processAttesterSlashings(ctx, attSlashings); err != nil {
+		log.WithError(err).Error("Could not process proposer slashings")
+		return
+	}
+}
+
+func (s *Service) waitForSync(ctx context.Context, genesisTime time.Time) {
 	if slotutil.SlotsSinceGenesis(genesisTime) == 0 || !s.serviceCfg.SyncChecker.Syncing() {
 		return
 	}
@@ -148,7 +218,7 @@ func (s *Service) waitForSync(genesisTime time.Time) {
 				continue
 			}
 			return
-		case <-s.ctx.Done():
+		case <-ctx.Done():
 			return
 		}
 	}
