6cc96db17f81828806df5d8b372c684fce6579c2
rauljordan <raul@prysmaticlabs.com>
2021-07-22 13:57:56 -0500

    begin moving state protos

--- proto/interfaces/metadata.go
+++ proto/interfaces/metadata.go
@@ -1,11 +1,10 @@
-package p2p
+package interfaces
 
 import (
 	"github.com/prysmaticlabs/go-bitfield"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
-// Metadata returns the interface of a p2p metadata type.
 type Metadata interface {
 	SequenceNumber() uint64
 	AttnetsBitfield() bitfield.Bitvector64
